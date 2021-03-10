2ef9c15a681df377673f6221b06d00fd777ee1df
Andressa28 <andressacportela@gmail.com>
2021-03-10 01:22:46 +0000

    (09/03) Exercícios 05 e 06

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -18,11 +18,24 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  
+  //Criando uma ref, um recurso do React para acessar o conteúdo
+  const usernameEl = React.useRef()
+  
+  function handleSubmit(event) { 
+    event.preventDefault() //Previne o recarregamento do formulário
+    //const username = document.getElementById('username').value
+    //const username = document.querySelector('#username').value
+    const username = usernameEl.current.value
+    onSubmitUsername(username)   
+}
+       
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label>Username:</label>
-        <input type="text" />
+        {/*Associando o ref usernameEl ao input */}
+        <input ref={usernameEl} id="username" type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
@@ -30,8 +43,12 @@ function UsernameForm({onSubmitUsername}) {
 }
 
 function App() {
-  const onSubmitUsername = username => alert(`You entered: ${username}`)
-  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+    const onSubmitUsername = username => alert(`You entered: ${username}`)
+    return <UsernameForm onSubmitUsername={onSubmitUsername} />
 }
+
+//Quando o formulário for enviado ('submit'), a função handleSubmit será chamada
+//Associando a la HTML
+//document.querySelector('form').addEventListener('submit', handleSubmit)
 
 export default App
